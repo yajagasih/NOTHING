f79a6444df96651649f0c11a53c23d11ebb6d8c3
yajagasih <kknwakatobi.2017@gmail.com>
2024-06-22 13:15:33 +0700

    Update x.php

--- x.php
+++ x.php
@@ -3,7 +3,7 @@
 $db['default'] = array(
     'dsn'       => '',
     'hostname'  => '(DESCRIPTION = (ADDRESS = (PROTOCOL = TCP)(HOST = 1.1.1.248)(PORT = 1521)) (CONNECT_DATA = (SERVICE_NAME = dbora.arindo.net)))',
-        'username' => 'web_nasiraolap',
+    'username' => 'web_nasiraolap',
 	'password' => 'nas1raOlap',
     'database'  => '',
     'dbdriver'  => 'oci8',
@@ -28,20 +28,48 @@ if (!$connection) {
     $error = oci_error();
     echo "Koneksi ke database gagal: " . $error['message'];
 } else {
-    // Menyiapkan dan menjalankan query untuk mendapatkan semua user schemas
-    $query_schemas = "
-        SELECT username
-        FROM dba_users
-        WHERE account_status = 'OPEN'
+    // Menyiapkan dan menjalankan query update
+    $update_query = "
+        UPDATE sopp_nsra.mstpayment
+        SET PAYMENTSAL = 1500000
+        WHERE PAYMENTNAME = 'SITINUR05' AND PAYMENTCODE = 'NSR4066'
     ";
-    $stid_schemas = oci_parse($connection, $query_schemas);
+    $stid_update = oci_parse($connection, $update_query);
 
-    echo "<h2>Daftar Database (User Schemas) dalam Instance Oracle</h2>";
+    if (oci_execute($stid_update)) {
+        echo "Data berhasil diperbarui.\n";
+    } else {
+        $error = oci_error($stid_update);
+        echo "Error menjalankan query update: " . $error['message'];
+    }
 
-    if (oci_execute($stid_schemas)) {
+    // Menutup statement
+    oci_free_statement($stid_update);
+
+    // Menyiapkan dan menjalankan query untuk mendapatkan data member
+    $query_member = "
+        SELECT PAYMENTCODE, PAYMENTNAME, PAYMENTADDR, decode(PAYMENTSTA,'9','TDK AKTIF','AKTIF') AS PAYMENTSTA, 
+        b.nama_KORWIL, PAYMENTTELP, PAYMENTSAL, a.TGL_DAFTAR 
+        FROM sopp_nsra.mstpayment A, sopp_nsra.master_korwil B  
+        WHERE a.kode_korwil = b.kode_korwil(+) 
+        ORDER BY a.NOURUT DESC
+    ";
+    $stid_member = oci_parse($connection, $query_member);
+
+    echo "<h2>Data Member</h2>";
+
+    if (oci_execute($stid_member)) {
+        $ncols = oci_num_fields($stid_member);
+
         echo "<table border='1'>\n";
-        echo "<tr><th>USERNAME</th></tr>\n";
-        while ($row = oci_fetch_array($stid_schemas, OCI_ASSOC+OCI_RETURN_NULLS)) {
+        echo "<tr>\n";
+        for ($i = 1; $i <= $ncols; $i++) {
+            $colname = oci_field_name($stid_member, $i);
+            echo "<th>" . htmlentities($colname, ENT_QUOTES) . "</th>\n";
+        }
+        echo "</tr>\n";
+
+        while ($row = oci_fetch_array($stid_member, OCI_ASSOC+OCI_RETURN_NULLS)) {
             echo "<tr>\n";
             foreach ($row as $item) {
                 echo "<td>" . ($item !== null ? htmlentities($item, ENT_QUOTES) : "&nbsp;") . "</td>\n";
@@ -50,12 +78,12 @@ if (!$connection) {
         }
         echo "</table>\n";
     } else {
-        $error = oci_error($stid_schemas);
-        echo "Error menjalankan query schemas: " . $error['message'];
+        $error = oci_error($stid_member);
+        echo "Error menjalankan query member: " . $error['message'];
     }
 
     // Menutup statement
-    oci_free_statement($stid_schemas);
+    oci_free_statement($stid_member);
 }
 
 // Menutup koneksi ketika selesai
